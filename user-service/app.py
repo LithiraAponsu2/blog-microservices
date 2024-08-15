27db129365330808aaa43ed97762741ec6d82464
LithiraAponsu2 <aponsulithiraabb@gmail.com>
2024-08-15 06:25:21 +0530

    changes

--- user-service/app.py
+++ user-service/app.py
@@ -2,12 +2,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/users', methods=['GET'])
 def get_users():
     return jsonify([
         {"id": 1, "username": "john_doe"},
         {"id": 2, "username": "jane_smith"}
     ])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
